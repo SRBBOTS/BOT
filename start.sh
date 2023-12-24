738885ff581362c03f6c29e8de08830c07a57aa8
SRBBOTS <154682068+SRBBOTS@users.noreply.github.com>
2023-12-24 06:24:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mrahesh24/BOT.git /BOT 
+  git clone https://github.com/SRBBOTS/BOT.git /BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /BOT 
